ca4ee03abd64964bcf6eb01424bb77d91b09836e
tiray3742-sys <tiray3742@gmail.com>
2026-01-15 09:11:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function order(produk) {
-    let pesan = `Halo, saya ingin memesan ${produk}. Mohon info detailnya ya 😊`;
+    let pesan = `Hallo kak, saya ingin memesan ${produk}. Mohon info detailnya ya 😊`;
     let wa = "https://wa.me/6281377747001?text=" + encodeURIComponent(pesan);
     window.open(wa, "_blank");
 }
+
